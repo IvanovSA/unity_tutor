c810b9104d1e964d365adb97b8840925859cb981
IvanovSA <ivs.mailg@gmail.com>
2017-02-12 17:50:03 +0300

    Create primitives

---
+++ Assets/Asserts/Scripts/cameraScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class cameraScript : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	Camera camera;
+
+	void Start() {
+		camera = GetComponent<Camera>();
+
+	}
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(new Vector3(target.position.x, target.position.y+0.75f,target.position.z));
+			Vector3 delta = new Vector3(target.position.x, target.position.y+0.75f,target.position.z) - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+}
---
+++ Assets/Asserts/Scripts/camera_move_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move_script : MonoBehaviour {
+	public float dampTime = 0.15f;
+	public float camSpeed = 1.0f;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (key: KeyCode.LeftArrow)) {
+			Vector3 destination = transform.position + new Vector3(-1.0f * camSpeed, 0.0f, 0.0f);
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+		if (Input.GetKey (key: KeyCode.RightArrow)) {
+			Vector3 destination = transform.position + new Vector3(camSpeed, 0.0f, 0.0f);
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	}
+}
